534b0a7ba01374a8918e6f9b3da2f2e1094c414b
Rafael Moreira de Melo <132792028+rafaelmelom-dev@users.noreply.github.com>
2023-06-19 23:03:38 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,25 @@
+const handleIntersection = (entries, observer) => {
+    entries.forEach(entry => {
+        if (entry.isIntersecting || entry.intersectionRatio > 0) {
+            entry.target.classList.add('active');
+            observer.unobserve(entry.target);
+        };
+    });
+};
+
+const observer = new IntersectionObserver(handleIntersection, {
+    root: null,
+    threshold: 1
+})
+
+
+document.addEventListener('DOMContentLoaded', () => {
+    const content = document.querySelectorAll('section')
+
+    content.forEach((item) => {
+        observer.observe(item)
+    });
+})
+
+
+
