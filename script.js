7e5469da8373337336904985b79cfd80370c96d9
Rafael Moreira de Melo <132792028+rafaelmelom-dev@users.noreply.github.com>
2023-06-27 19:29:54 -0300

    Add files via upload

--- script.js
+++ script.js
@@ -22,5 +22,114 @@ document.addEventListener('DOMContentLoaded', () => {
     });
 })
 
+const refresh = () => {
+    document.getElementsByTagName('section')[0].scrollIntoView({behavior:"smooth"})
+}
 
 
+
+
+let url = "https://raw.githubusercontent.com/rafaelmelom-dev/diabete_website/main/diabetes.json"
+
+fetch(url)
+.then(res => res.json())
+.then(data => {
+  var labels = []
+   
+  for (let i = 0; i < data.Pregnancies.length; i ++) {
+
+    if (!labels.includes(data.Pregnancies[i])) {
+      labels.push(data.Pregnancies[i])
+    }
+    labels = labels.sort((a, b) => a - b)
+
+  };
+
+  const OutcomeData = {0:[], 1:[]};
+
+  const filterData = (out, preg) => {
+
+    const filteredData = data.Pregnancies.filter((_, index) => {
+      return data.Outcome[index] === out && data.Pregnancies[index] === preg;
+    });
+
+    return filteredData.length
+  }
+
+  labels.forEach((i) => {
+    for (let j = 0; j < 2; j ++) {
+      OutcomeData[j].push(filterData(j, i))
+    }
+  })
+  
+  
+  var screenWidth = window.innerWidth || document.documentElement.clientWidth || document.body.clientWidth;
+  
+  const setup = {
+  type:'bar',
+  data: {
+    labels: labels,
+    datasets: [
+      {
+        label: "Não-diabetes(0)",
+        data: OutcomeData[0],
+        backgroundColor: "rgba(149, 55, 204, 0.7)",
+        borderColor: "rgba(229, 52, 235, 1)"
+      },
+      {
+        label:"Diabetes(1)",
+        data: OutcomeData[1],
+        backgroundColor: "rgba(229, 52, 235, 0.7)",
+        borderColor: "rgba(149, 55, 204, 1)"
+      }
+    ]
+  },
+  options: {
+    indexAxis:'y',
+    responsive: true,
+    plugins: {
+      legend : {
+        position: "bottom"
+      },
+      title :{
+        display: true,
+        text: "Pregnancies by Outcome",
+        color:'rgb(149, 55, 204)'
+      },
+      tooltip:{
+        callbacks :{
+          title: function(item, data) {
+            return "Gravidez: " + item[0].label
+          }
+        }
+      }
+    },
+    maintainAspectRatio: false,
+    scales: {
+      x: {
+        display: true,
+        scaleLabel: {
+          display: true,
+          labelString: 'X Label'
+        }
+      },
+      y: {
+        display: true,
+        scaleLabel: {
+          display: true,
+          labelString: 'Y Label'
+        }
+      }
+    }
+  }
+}
+
+if (screenWidth > 550) {
+  setup.options.indexAxis = null
+}
+
+const myChart = new Chart(
+  document.getElementById('chartdiv').getContext('2d'),
+  setup
+  )
+});
